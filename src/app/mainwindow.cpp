543cf432ee9741eb3e4bc16dfebc901a15f9091c
lanhuidong <472897576@qq.com>
2023-07-16 22:06:10 +0800

    feat: 创建菜单栏

--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -1,5 +1,22 @@
 #include "mainwindow.h"
 
-MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {}
+#include <QMenuBar>
+
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
+    setWindowTitle("xPhoto");
+    setWindowState(Qt::WindowMaximized);
+
+    file_menu_ = menuBar()->addMenu(tr("&File"));
+    open_action_ = new QAction(tr("&Open Folder"), this);
+    file_menu_->addAction(open_action_);
+
+    view_menu_ = menuBar()->addMenu(tr("&View"));
+    status_bar_action_ = new QAction(tr("&show status bar"), this);
+    view_menu_->addAction(status_bar_action_);
+
+    help_menu_ = menuBar()->addMenu(tr("&Help"));
+    about_action_ = new QAction(tr("&About"), this);
+    help_menu_->addAction(about_action_);
+}
 
 MainWindow::~MainWindow() {}
--- src/app/mainwindow.h
+++ src/app/mainwindow.h
@@ -1,7 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QAction>
 #include <QMainWindow>
+#include <QMenu>
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -9,5 +11,15 @@ class MainWindow : public QMainWindow {
    public:
     MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
+
+   private:
+    QMenu* file_menu_;
+    QAction* open_action_;
+
+    QMenu* view_menu_;
+    QAction* status_bar_action_;
+
+    QMenu* help_menu_;
+    QAction* about_action_;
 };
 #endif  // MAINWINDOW_H
